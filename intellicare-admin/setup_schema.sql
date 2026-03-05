2f3cd66547da582ec7d987dc9fd17ff31c935140
Eduardo Garabini <egarabini@intellicare>
2026-03-04 21:29:29 -0300

    chore: update

--- intellicare-admin/setup_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS platform;
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'admin_module_role') THEN
-        CREATE ROLE admin_module_role;
-    END IF;
-END $$;
-GRANT ALL ON SCHEMA platform TO admin_module_role;
